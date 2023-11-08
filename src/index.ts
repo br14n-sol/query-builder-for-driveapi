49a6e8519ffeabb611d6dc96e6dfaa80dae1bad7
Niall <niall@n-mcd.co.uk>
2023-11-08 21:04:49 +0000

    Updated return types to improve chaining

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,99 @@
+type QueryBuilderTermMethods = Pick<QueryBuilder, 'inParents' | 'name' | 'fullText' | 'mimeType' | 'modifiedTime' | 'createdTime'>
+
 class QueryBuilder {
   private readonly queries: string[] = []
   private negateNextTerm: boolean = false
   private lastTerm: string = ''
 
-  private addInput (operator: string, value: string): void {
+  private addInput(operator: string, value: string): void {
     let query = this.negateNextTerm ? 'not ' : ''
     query += `${this.lastTerm} ${operator} '${value}'`
 
     this.queries.push(query)
   }
 
-  not (): this {
+  // Comparison methods
+
+  not(): QueryBuilderTermMethods {
     this.negateNextTerm = true
     return this
   }
 
-  contains (value: string): this {
+  contains(value: string): QueryBuilderTermMethods {
     this.addInput('contains', value)
 
     return this
   }
 
-  isEqualTo (value: string): this {
+  isEqualTo(value: string): QueryBuilderTermMethods {
     this.addInput('=', value)
 
     return this
   }
 
-  isNotEqualTo (value: string): this {
+  isNotEqualTo(value: string): QueryBuilderTermMethods {
     this.addInput('!=', value)
 
     return this
   }
 
-  isLessThan (value: string): this {
+  isLessThan(value: string): QueryBuilderTermMethods {
     this.addInput('<', value)
 
     return this
   }
 
-  isLessThanOrEqualTo (value: string): this {
+  isLessThanOrEqualTo(value: string): QueryBuilderTermMethods {
     this.addInput('<=', value)
 
     return this
   }
 
-  isGreaterThan (value: string): this {
+  isGreaterThan(value: string): QueryBuilderTermMethods {
     this.addInput('>', value)
 
     return this
   }
 
-  isGreaterThanOrEqualTo (value: string): this {
+  isGreaterThanOrEqualTo(value: string): QueryBuilderTermMethods {
     this.addInput('>=', value)
 
     return this
   }
 
-  inParents (folderId: string): this {
+  // Term methods
+
+  inParents(folderId: string): QueryBuilderTermMethods {
     this.queries.push(`'${folderId}' in parents`)
     return this
   }
 
-  name (): this {
+  name(): Pick<this, 'contains' | 'isEqualTo' | 'isNotEqualTo'> {
     this.lastTerm = 'name'
     return this
   }
 
-  fullText (): this {
+  fullText(): Pick<this, 'contains'> {
     this.lastTerm = 'fullText'
     return this
   }
 
-  mimeType (): this {
+  mimeType(): Pick<this, 'contains' | 'isEqualTo' | 'isNotEqualTo'> {
     this.lastTerm = 'mimeType'
     return this
   }
 
-  modifiedTime (): this {
+  modifiedTime(): Pick<this, 'isLessThan' | 'isLessThanOrEqualTo' | 'isEqualTo' | 'isNotEqualTo' | 'isGreaterThanOrEqualTo' | 'isGreaterThan'> {
     this.lastTerm = 'modifiedTime'
     return this
   }
 
-  createdTime (): this {
+  createdTime(): Pick<this, 'isLessThan' | 'isLessThanOrEqualTo' | 'isEqualTo' | 'isNotEqualTo' | 'isGreaterThanOrEqualTo' | 'isGreaterThan'> {
     this.lastTerm = 'createdTime'
     return this
   }
 
-  build (): string {
+  build(): string {
     return this.queries.join(' and ')
   }
 }

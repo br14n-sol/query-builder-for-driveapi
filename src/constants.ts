5f402c9ade1f948dbf4018708ef0e0ff69cd35bc
Brian Fernandez <br14n_sol@protonmail.com>
2023-12-29 21:53:06 -0500

    refactor: remove unused values from `Operator` enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,5 @@
 export enum Operator {
-  CONTAINS = 'contains',
   EQUAL = '=',
-  UNEQUAL = '!=',
-  LESS_THAN = '<',
-  LESS_THAN_OR_EQUAL = '<=',
-  GREATER_THAN = '>',
-  GREATER_THAN_OR_EQUAL = '>=',
   IN = 'in',
   AND = 'and',
   OR = 'or',

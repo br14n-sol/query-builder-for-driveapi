76a7aa8a0d93feb394f5bebf1dfd3749237c5f65
Brian Fernandez <br14n_sol@protonmail.com>
2024-05-11 19:57:08 +0000

    Revert "build(deps-dev): remove `fix-tsup-cjs`" This reverts commit 0bf92403858c0a608a6caf96fb752be2dc781366.

--- fix-cjs-output.js
+++ fix-cjs-output.js
@@ -1,103 +1,43 @@
+import { fixCjsExports, fixCjsDts } from 'fix-tsup-cjs'
 import { minify } from 'terser'
 
 import path from 'node:path'
 import fs from 'node:fs/promises'
 
-const statements = `
-if (module.exports.default) {
-  Object.assign(module.exports.default, module.exports);
-  module.exports = module.exports.default;
-  delete module.exports.default;
-}
-`
+const { outDir } = (
+  await import('./tsup.config.json', {
+    with: { type: 'json' }
+  })
+).default
+const currentDir = process.cwd()
+const fixDir = path.resolve(currentDir, outDir)
 
-async function main() {
-  const { outDir } = (
-    await import('./tsup.config.json', {
-      with: { type: 'json' }
-    })
-  ).default
-  const currentDir = process.cwd()
-  const outDirPath = path.resolve(currentDir, outDir)
+await fixCjsExports({ globOptions: { cwd: fixDir } })
+await fixCjsDts({ globOptions: { cwd: fixDir } })
 
-  const files = await getFiles(outDirPath)
-  for (const file of files) {
-    const code = await fs.readFile(file.path, 'utf-8')
-    let result
+const fixFiles = await fs.readdir(fixDir)
+for (const file of fixFiles) {
+  if (/^.+\.(js|cjs)$/.test(file)) {
+    const filePath = path.resolve(fixDir, file)
+    const { size: oldSize } = await fs.stat(filePath)
 
-    if (/^.+\.cjs$/.test(file.name)) {
-      result = fixExports(code)
-      logger('fix-cjs-exports', `${result ? '✔' : 'skip'} ${file.name}`)
-    }
+    const code = await fs.readFile(filePath, 'utf-8')
+    const result = await minify(code)
 
-    if (/^.+\.(js|cjs)$/.test(file.name)) {
-      result = (await minify(result ?? code)).code
-      const size = Buffer.byteLength(result)
-      logger(
-        'minify',
-        path.relative(currentDir, file.path),
-        `${formatBytes(file.size)} ➡ ${formatBytes(size)}`
-      )
-    }
+    await fs.writeFile(filePath, result.code ?? '')
 
-    if (/^.+\.cts$/.test(file.name)) {
-      result = fixTypes(code)
-      logger('fix-cjs-types', `${result ? '✔' : 'skip'} ${file.name}`)
-    }
+    const { size: newSize } = await fs.stat(filePath)
 
-    if (result) {
-      await fs.writeFile(file.path, result)
-    }
+    console.log(
+      '[minify]',
+      path.join(outDir, file),
+      formatBytes(oldSize),
+      '➡',
+      formatBytes(newSize)
+    )
   }
-}
-
-async function getFiles(directory) {
-  const filenames = await fs.readdir(directory)
-  const output = []
-
-  for (const file of filenames) {
-    const filePath = path.join(directory, file)
-    const fileStat = await fs.stat(filePath)
-
-    if (fileStat.isDirectory()) {
-      output.push(...(await getFiles(filePath)))
-    } else {
-      output.push({
-        name: file,
-        path: filePath,
-        size: fileStat.size
-      })
-    }
-  }
-
-  return output
-}
-
-function fixExports(code) {
-  if (
-    code.includes('module.exports = __toCommonJS') &&
-    !code.endsWith(statements)
-  ) {
-    return code + statements
-  }
-}
-
-function fixTypes(code) {
-  const result = /export \{.*\b(?<name>.+) as default.*\};/s.exec(code)
-  if (result.groups.name) {
-    const statement = `export = ${result.groups.name}`
-    if (!code.endsWith(statement)) {
-      return code + statement
-    }
-  }
-}
-
-function logger(name, ...messages) {
-  console.log(`[${name}]`, ...messages)
 }
 
 function formatBytes(bytes) {
   return `${(bytes / 1024).toFixed(2)} KB`
 }
-
-await main()
